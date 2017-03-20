c055a1adb9049c87c99f48f5dee8569a57d388b5
segawasai <segawasai@gmail.com>
2017-03-20 16:03:33 +0700

    finish food and user

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -12,4 +12,42 @@ class FoodsController < ApplicationController
     @foods = Food.all
   end
 
+  def edit
+    @food = Food.find(params[:id])
+  end
+
+  def update
+    @food = Food.find(params[:id])
+    if @food.update_attributes(food_params)
+      flash[:success] = "Food updated"
+      render 'show'
+    else
+      render 'edit'
+    end
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+    	flash[:success] = "Adding successful"
+    	redirect_to @food
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+    def destroy
+    Food.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to '/foods'
+  end
+
+  private 
+
+  def food_params
+      params.require(:food).permit(:name, :price, :url)
+    end
+
+
 end
